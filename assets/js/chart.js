c27339882be91f6e1cca42012d6e25b80c06e02e
1782777 <771279043@qq.com>
2020-05-24 01:13:29 +0800

    add data

--- assets/js/chart.js
+++ assets/js/chart.js
@@ -1,6 +1,52 @@
+var volume_mean = new Array(244).fill(0);
 
-function myChart(id){
-    var myChart = echarts.init(document.getElementById(id));
+function init(){
+    $.ajax({
+        type : "get",
+        async : true,            //异步请求（同步请求将会锁住浏览器，用户其他操作必须等待请求完成才可以执行）
+        url : "http://img1.money.126.net/data/hs/time/4days/0000016.json",    //请求发送到TestServlet处
+        data : {},
+        dataType : "jsonp",        //返回数据形式为json
+        success : function(data) {
+            //请求成功时执行该函数内容，result即为服务器返回的json对象
+            chart_volume.hideLoading();
+            obj = JSON.parse(JSON.stringify(data))
+            
+            // console.log(obj.data.length);
+
+            for(var k=0;k<4;k++)
+            {
+                // console.log(obj.data[k].data.length);
+                var tmp = new Array();
+                for (var i=0;i<obj.data[k].data.length;i++)
+                { 
+                    //console.log(obj.data[k].data[i][3]);
+                    var v = obj.data[k].data[i][3];
+                    if(i==241){console.log(v);}
+                    volume_mean[i] = volume_mean[i]+v/4;
+                    if(i==241){console.log(volume_mean[i]);}
+                    // console.log(v);
+                    // volume.push(v);
+                    
+                }
+            }
+            
+            //console.log(volume_mean);
+            
+        },
+        error : function(errorMsg) {
+            //请求失败时执行该函数
+            alert("图表请求数据失败!");
+            chart_volume.hideLoading();
+        }
+    })
+}
+
+
+
+var chart_north;
+function Chart_north(id){
+    chart_north = echarts.init(document.getElementById(id));
             
     // 指定图表的配置项和数据
     var option = {
@@ -10,69 +56,174 @@ function myChart(id){
             fontSize:14
         }      
     },
-    color:["#61a0a8"],
+    color:["red","blue","grey"],
     tooltip: {},
     legend: {},
     toolbox: {},      
     xAxis: [{         
-        data: ['周一', '周二', '周三', '周四', '周五', '周六', '周日']
+        data: []
     }],
     yAxis: { },
     series: [{
-        name: 'count',
+        name: 'hgt',
         type: 'line',
-        data: [11, 11, 15, 13, 12, 13, 100] , 
-        areaStyle: {}       
+        data: []
+        //areaStyle: {}       
     },
     {
-        name: 'count',
+        name: 'sgt',
         type: 'line',
-        data: [1, -2, 2, 5, 3, 2, 0]        
+        data: []        
+    },
+    {
+        name: 'north',
+        type: 'line',
+        data: []        
     }]
     };
     // 使用刚指定的配置项和数据显示图表。
-    myChart.setOption(option);
+    Load_north()
+    setInterval(function(){Load_north()}, 10000); 
+    chart_north.setOption(option);
 }
 
-function myChart2(id){
-    var myChart = echarts.init(document.getElementById(id));
+function Load_north()
+{
+    $.ajax({
+        type : "get",
+        async : true,            //异步请求（同步请求将会锁住浏览器，用户其他操作必须等待请求完成才可以执行）
+        url : "http://push2.eastmoney.com/api/qt/kamt.rtmin/get?fields1=f1&fields2=f51,f52,f54,f56&ut=b2884a393a59ad64002292a3e90d46a5",    //请求发送到TestServlet处
+        data : {},
+        dataType : "json",        //返回数据形式为json
+        success : function(data) {
+            //请求成功时执行该函数内容，result即为服务器返回的json对象
+            chart_north.hideLoading();
+            obj = JSON.parse(JSON.stringify(data))
+            var n1 = new Array();
+            var n2 = new Array();
+            var n3 = new Array();
+            for (var i=0;i<obj.data.s2n.length;i++)
+            { 
+                var three = obj.data.s2n[i].split(',');
+                //console.log(three);
+                n1.push(three[1]);
+                n2.push(three[2]);
+                n3.push(three[3]);
+            }
+            // console.log(n1);
+            if (data) {
+                chart_north.setOption({
+                    
+                    series: [{
+                        name: 'hgt',
+                        data: n1
+                    },
+                    {
+                        name: 'sgt',
+                        data: n2
+                    },
+                    {
+                        name: 'north',
+                        data: n3
+                    }]
+                });
+            }
+        },
+        error : function(errorMsg) {
+            //请求失败时执行该函数
+            alert("图表请求数据失败!");
+            chart_north.hideLoading();
+        }
+   })
+}
+
+var chart_volume;
+function Chart_volume(id){
+    chart_volume = echarts.init(document.getElementById(id));
             
     // 指定图表的配置项和数据
     var option = {
-    title: {
-        text: '我的体重',     
-        textStyle:{
-            fontSize:14
-        }
+        title: {
+            text: '我的体重',     
+            textStyle:{
+                fontSize:14
+            }
 
-    },
-    color:["#61a0a8"],
-    tooltip: {},
-    legend: {},
-    toolbox: {},      
-    xAxis: [{         
-        data: ['1', '2', '3', '4', '5', '6', '7']
-    }],
-    yAxis: { },
-    series: [{
-        name: 'weight',
-        type: 'line',
-        data: [11, 17, 25, 23, 22, 523, 20] ,
-        smooth: true         
-    },
-    {
-        name: 'weight',
-        type: 'line',
-        data: [1, -2, 2, 5, 3, 2, 0]        
-    }]
+        },
+        color:["#61a0a8"],
+        tooltip: {},
+        legend: {},
+        toolbox: {},      
+        xAxis: [{         
+            data: []
+        }],
+        yAxis: { },
+        series: [{
+            name: 'diff',
+            type: 'line',
+            data: [] ,
+            // smooth: true         
+        }]
     };
     // 使用刚指定的配置项和数据显示图表。
-    myChart.setOption(option);
+    chart_volume.setOption(option);
+    Load_volume()
+    setInterval(function(){Load_volume()}, 10000); 
 }
 
+function Load_volume()
+{
+    $.ajax({
+        type : "get",
+        async : true,            //异步请求（同步请求将会锁住浏览器，用户其他操作必须等待请求完成才可以执行）
+        url : "http://img1.money.126.net/data/hs/time/today/0000016.json",    //请求发送到TestServlet处
+        data : {},
+        dataType : "jsonp",        //返回数据形式为json
+        success : function(data) {
+            //请求成功时执行该函数内容，result即为服务器返回的json对象
+            chart_volume.hideLoading();
+            obj = JSON.parse(JSON.stringify(data))
+            var volume = new Array();
+            var diff = new Array();
+            console.log(obj.data.length);
+            for (var i=0;i<obj.data.length;i++)
+            { 
+                // console.log(obj.data[i][3]);
+                var v = obj.data[i][3];
+                //console.log(three);
+                volume.push(v);
+                
+            }
+            console.log(volume);
+            for (var i=0;i<volume.length;i++)
+            {
+                var d = volume[i]/volume_mean[i];
+                diff.push(d);
+            }
+            //console.log(volume);
+            if (data) {
+                chart_volume.setOption({
+                    series: [{
+                        name: 'diff',
+                        data: diff
+                    }]
+                });
+            }
+        },
+        error : function(errorMsg) {
+            //请求失败时执行该函数
+            alert("图表请求数据失败!");
+            chart_volume.hideLoading();
+        }
+   })
+}
+
+
+var CharIVX ;
 function Bar(id){
-    var myChart = echarts.init(document.getElementById(id));
-    myChart.setOption({
+    CharIVX = echarts.init(document.getElementById(id));
+    //var myChart = echarts.init(document.getElementById(id));
+    CharIVX.setOption({
         title: {
             text: '异步数据加载示例',
             textStyle:{
@@ -88,16 +239,18 @@ function Bar(id){
         },
         yAxis: {},
         series: [{
-            name: '销量',
-            type: 'bar',
-            data: [1,2,3,4,5,6,7]
+            name: 'cout',
+            type: 'line',
+            data: []
         }]
     });
-    myChart.showLoading();
-
-
     
+    Load_QVIX()
+    setInterval(function(){Load_QVIX()}, 10000); 
+}
 
+function Load_QVIX()
+{
     $.ajax({
         type : "get",
         async : true,            //异步请求（同步请求将会锁住浏览器，用户其他操作必须等待请求完成才可以执行）
@@ -106,49 +259,23 @@ function Bar(id){
         dataType : "json",        //返回数据形式为json
         success : function(data) {
             //请求成功时执行该函数内容，result即为服务器返回的json对象
-            myChart.hideLoading();
-            console.log(11111);
+            CharIVX.hideLoading();
+            obj = JSON.parse(JSON.stringify(data))
+            console.log(obj.iv);
             if (data) {
-                myChart.setOption({
-                    xAxis: {
-                        data: data.time
-                        
-                    },
+                CharIVX.setOption({
                     series: [{
                         // 根据名字对应到相应的系列
                         name: '销量',
                         data: data.iv
-                        
                     }]
                 });
-                   
             }
-        
-       },
+        },
         error : function(errorMsg) {
             //请求失败时执行该函数
             alert("图表请求数据失败!");
-            myChart.hideLoading();
+            CharIVX.hideLoading();
         }
    })
-
-    
-    // 异步加载json格式数据
-    // $.getJSON('http://127.0.0.1:8080/json',function(data){
-    //     console.log(data.iv);
-    //     console.log("1111111111111111111");
-    //     myChart.setOption({
-    //         xAxis: {
-    //             data: data.time
-                
-    //         },
-    //         series: [{
-    //             // 根据名字对应到相应的系列
-    //             name: '销量',
-    //             data: data.iv
-                
-    //         }]
-    //     });
-        
-    // });
 }
--- make_data/mysqltest.py
+++ make_data/mysqltest.py
@@ -1,14 +1,15 @@
 import pymysql
 import datetime
 import random
+import time
 
 def make_tabel():
-    db_conn = pymysql.connect(host = 'localhost', user= 'root', passwd = 'zx111111',
+    db_conn = pymysql.connect(host = 'localhost', user= 'root', passwd = '123456',
                             database='option_data',port=3306,charset='utf8')
     cursor = db_conn.cursor()
-    d = datetime.datetime(2017, 8, 18, 9, 30, 0)
-    for i in range(1440):
-        d =  d+datetime.timedelta(seconds=5)
+    d = datetime.datetime(2017, 8, 18, 9, 29, 0)
+    for i in range(122):
+        d =  d+datetime.timedelta(minutes=1)
         dd =d.strftime('%H:%M:%S')
         print(dd)
         iv = random.randint(0,9)*2.5
@@ -17,9 +18,9 @@ def make_tabel():
         
         cursor.execute(sql,dd)
 
-    d = datetime.datetime(2017, 8, 18, 13, 0, 0)
-    for i in range(1440):
-        d =  d+datetime.timedelta(seconds=5)
+    d = datetime.datetime(2017, 8, 18, 12, 59, 0)
+    for i in range(122):
+        d =  d+datetime.timedelta(minutes=1)
         dd =d.strftime('%H:%M:%S')
         print(dd)
         iv = random.randint(0,9)*2.5
@@ -31,6 +32,18 @@ def make_tabel():
 
     db_conn.close()
 
+def insert_test():
+    db_conn = pymysql.connect(host = 'localhost', user= 'root', passwd = '123456',
+                            database='option_data',port=3306,charset='utf8')
+    cursor = db_conn.cursor()
+    for i in range(1000):
+        sql = 'update iv_mean set iv = %s where id = %s'
+        print(i%244)
+        cursor.execute(sql,[random.randint(1,100),i%244])
+        db_conn.commit()
+        time.sleep(0.5)
+    db_conn.close()
 
 if __name__ == '__main__':
+    insert_test()
     pass

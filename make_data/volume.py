d0d00943e86f37ebb3d474ad01b76667dbc8bfd6
zx <771279043@qq.com>
2021-05-27 14:22:14 +0800

    fix volume cumsum

--- make_data/volume.py
+++ make_data/volume.py
@@ -106,7 +106,8 @@ class volume:
             today_list.append(int_vol)
         print(today_list)
         pf_volume['today'] = today_list
-
+        pf_volume= pf_volume.cumsum()
+        print(pf_volume)
         pf_volume['res'] = pf_volume['today'] / pf_volume['Col_sum']
         # pf_volume['res'].plot()
         # print(pf_volume)
@@ -114,7 +115,8 @@ class volume:
         df_sql = pd.DataFrame()
         df_sql['id'] = pf_volume.index
         df_sql['volume'] = pf_volume['res']
-        print(df_sql)
+        
+        # print(df_sql)
         engine = sqlalchemy.create_engine('mysql+pymysql://root:root@localhost/option_data?charset=utf8')
         df_sql.to_sql('volume', engine, index=False, if_exists='replace')
         #http://push2.eastmoney.com/api/qt/stock/trends2/get?secid=1.000016&fields1=f1,f2,f3,f4,f5,f6,f7,f8,f9,f10,f11,f12,f13&fields2=f51,f52,f53,f54,f55,f56,f57,f58&ut=fa5fd1943c7b386f172d6893dbfba10b
